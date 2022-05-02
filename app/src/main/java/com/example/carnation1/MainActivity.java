83f2d890195fc6daca4bbeca3d8557c372147887
admi31 <qnco489@naver.com>
2022-05-02 14:18:55 +0900

    서버 IP 포트 번호

--- app/src/main/java/com/example/carnation1/MainActivity.java
+++ app/src/main/java/com/example/carnation1/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /* IP: 104.197.76.225
+     *  포트 번호는 7030
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

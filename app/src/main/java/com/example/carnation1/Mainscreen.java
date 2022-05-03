10edfbab967748c5f150c6c999aeab537c1db04d
tae993136 <tae993136@gmail.com>
2022-05-03 15:39:51 +0900

    alert dialog 마이페이지 이동

--- app/src/main/java/com/example/carnation1/Mainscreen.java
+++ app/src/main/java/com/example/carnation1/Mainscreen.java
@@ -1,9 +1,11 @@
 package com.example.carnation1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Mainscreen extends AppCompatActivity {
@@ -20,8 +22,23 @@ public class Mainscreen extends AppCompatActivity {
         });
         Button mypage_button = (Button) findViewById(R.id.mypage_button);
         mypage_button.setOnClickListener(v->{
-            Intent intent = new Intent(Mainscreen.this,Mypage.class);
-            startActivity(intent);
+
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Mainscreen.this);
+                builder.setTitle("    ");
+                builder.setMessage("마이페이지로 이동하시겠습니까?");
+                builder.setPositiveButton("아니오",null);
+                builder.setNegativeButton("예",   new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(Mainscreen.this,Mypage.class);
+                        startActivity(intent);
+                    }
+                });
+
+                builder.create().show();
+
         });
     }
 

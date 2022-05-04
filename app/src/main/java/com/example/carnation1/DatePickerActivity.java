8f11368fddcd330e140d93d1ffecf20440ab5c11
admi31 <qnco489@naver.com>
2022-05-04 11:06:08 +0900

    예약 오류

--- app/src/main/java/com/example/carnation1/DatePickerActivity.java
+++ app/src/main/java/com/example/carnation1/DatePickerActivity.java
@@ -1,6 +1,7 @@
 package com.example.carnation1;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -9,14 +10,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TimePicker;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 
 public class DatePickerActivity extends AppCompatActivity {
     private TimePicker timePicker;
     private TextView textView;
-    //private int mHour = 0, mMin = 0;
+    private int mHour = 0, mMin = 0;
     private int mYear = 0, mMonth = 0, mDay = 0;
 
     @Override
@@ -27,21 +27,28 @@ public class DatePickerActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_date_picker);
         Calendar calendar = new GregorianCalendar();
-        Date date = new Date();
+
 
         mYear = calendar.get(Calendar.YEAR);
         mMonth = calendar.get(Calendar.MONTH);
         mDay = calendar.get(Calendar.DAY_OF_MONTH);
         textView = findViewById(R.id.textView);
-        timePicker = findViewById(R.id.TimePicker);
+        timePicker = (TimePicker) findViewById(R.id.TimePicker);
 
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            mHour = timePicker.getHour();
+            mMin = timePicker.getMinute();
+        }else{
+            mHour = timePicker.getCurrentHour();
+            mMin = timePicker.getCurrentMinute();
+        }
 
         DatePicker datePicker = findViewById(R.id.vDatePicker);
         datePicker.init(mYear, mMonth, mDay, mOnDateChangedListener);
 
         timePicker.setOnTimeChangedListener (new TimePicker.OnTimeChangedListener() {
             @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+            public void onTimeChanged(TimePicker view,int hourOfDay, int minute) {
 
                 textView.setText(hourOfDay + "시" + minute + "분");
             }

ea5e86c73c41f6d87d3d2ddc35436783f9e9d28f
admi31 <qnco489@naver.com>
2022-05-04 10:46:11 +0900

    시간 UI 구현 성공

--- app/src/main/java/com/example/carnation1/DatePickerActivity.java
+++ app/src/main/java/com/example/carnation1/DatePickerActivity.java
@@ -5,16 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.DatePicker;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TimePicker;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 
 public class DatePickerActivity extends AppCompatActivity {
-
+    private TimePicker timePicker;
+    private TextView textView;
+    //private int mHour = 0, mMin = 0;
     private int mYear = 0, mMonth = 0, mDay = 0;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +27,29 @@ public class DatePickerActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_date_picker);
         Calendar calendar = new GregorianCalendar();
+        Date date = new Date();
 
         mYear = calendar.get(Calendar.YEAR);
         mMonth = calendar.get(Calendar.MONTH);
         mDay = calendar.get(Calendar.DAY_OF_MONTH);
+        textView = findViewById(R.id.textView);
+        timePicker = findViewById(R.id.TimePicker);
+
 
         DatePicker datePicker = findViewById(R.id.vDatePicker);
         datePicker.init(mYear, mMonth, mDay, mOnDateChangedListener);
 
+        timePicker.setOnTimeChangedListener (new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+
+                textView.setText(hourOfDay + "시" + minute + "분");
+            }
+        });
+
+
     }
+
 
 
     public void mOnClick(View v) {
@@ -56,5 +74,6 @@ public class DatePickerActivity extends AppCompatActivity {
         }
 
     };
+
 
 }

dd0c6ccf5608e29cc49d7cb043d48e544ecc4c0b
tae993136 <tae993136@gmail.com>
2022-05-04 10:23:51 +0900

    주차 예약날짜 선택

--- app/src/main/java/com/example/carnation1/DatePickerActivity.java
+++ app/src/main/java/com/example/carnation1/DatePickerActivity.java
@@ -12,74 +12,47 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 
-
 public class DatePickerActivity extends AppCompatActivity {
 
-
-
-    private int mYear =0, mMonth=0, mDay=0;
-
-
-
+    private int mYear = 0, mMonth = 0, mDay = 0;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_date_picker);
-
-
         Calendar calendar = new GregorianCalendar();
 
         mYear = calendar.get(Calendar.YEAR);
-
         mMonth = calendar.get(Calendar.MONTH);
-
         mDay = calendar.get(Calendar.DAY_OF_MONTH);
 
-
-
         DatePicker datePicker = findViewById(R.id.vDatePicker);
-
-        datePicker.init(mYear, mMonth, mDay,mOnDateChangedListener);
+        datePicker.init(mYear, mMonth, mDay, mOnDateChangedListener);
 
     }
 
 
-
-    public void mOnClick(View v){
-
+    public void mOnClick(View v) {
         Intent intent = new Intent();
-
-        intent.putExtra("mYear",mYear);
-
+        intent.putExtra("mYear", mYear);
         intent.putExtra("mMonth", mMonth);
-
         intent.putExtra("mDay", mDay);
-
         setResult(RESULT_OK, intent);
-
         finish();
-
     }
 
 
-
-    DatePicker.OnDateChangedListener mOnDateChangedListener = new DatePicker.OnDateChangedListener(){
+    DatePicker.OnDateChangedListener mOnDateChangedListener =
+            new DatePicker.OnDateChangedListener() {
 
         @Override
 
         public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
-
             mYear = yy;
-
             mMonth = mm;
-
             mDay = dd;
-
         }
 
     };

9cfa2d28746f36aac0939c306ae490514932cd96
Strange-1 <strange_1@daum.net>
2022-05-12 13:55:12 +0900

    rollback customView

--- app/src/main/java/com/example/carnation1/MyPage.java
+++ app/src/main/java/com/example/carnation1/MyPage.java
@@ -2,14 +2,10 @@ package com.example.carnation1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPage extends AppCompatActivity {
-    private TextView textView_ReservationStatus;
-    private TextView textView_ReservationDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +19,5 @@ public class MyPage extends AppCompatActivity {
         String msg_Hour = intent.getStringExtra("mHour") + "시 ";
         String msg_Minute = intent.getStringExtra("mMin") + "분";
         String msg = msg_Year + msg_Month + msg_Day + msg_Hour + msg_Minute;
-
-        textView_ReservationStatus = findViewById(R.id.myPage_TextReservationInfo_Status);
-        textView_ReservationStatus.setText("예약 중");
-        textView_ReservationDateTime = findViewById(R.id.myPage_TextReservationInfo_DateTime);
-        textView_ReservationDateTime.setText(msg);
-        findViewById(R.id.myPage_BackButton).setOnClickListener(v -> {
-            Intent intent1 = new Intent(MyPage.this, MainScreen.class);
-            startActivity(intent1);
-        });
     }
 }
